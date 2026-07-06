87e04527cd81ce2c39ca22eeff7c5141d4d5aeee
luchersou <195580638+luchersou@users.noreply.github.com>
2026-07-05 23:17:14 -0300

    chore: remove unused section and bento components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,10 @@ import { CTASection } from "@/components/sections/CTA-section";
 import { FeaturesSection } from "@/components/sections/features/features-section";
 import { Footer } from "@/components/sections/footer-section";
 import { Hero } from "@/components/sections/hero";
-import { FeaturesBentoSection } from "@/components/sections/feature-bento-section";
+import { FeaturesBento } from "@/components/sections/features-bento/features-bento-section";
 import { PricingSection } from "@/components/sections/pricing/pricing-section";
 import { StatsSection } from "@/components/sections/stats-section";
 import { TestimonialsSection } from "@/components/sections/testimonials-section";
-import { FeaturesBento } from "@/components/sections/features-bento/features-bento-section";
 
 export default function Home() {
   return (
